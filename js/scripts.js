4f6e3b37deb0547a0ac033876cb3de10a519f082
Viplove Prakash <viploveprakash@gmail.com>
2019-03-05 03:21:19 -0500

    Version 1.0.0

--- js/scripts.js
+++ js/scripts.js
@@ -10,36 +10,94 @@ function displayTimeOfDay() {
 }
 
 const greeting = document.getElementById('greeting');
-greeting.textContent = `Good ${displayTimeOfDay()}, Breezy!`;
-
-fetch("https://quotes.rest/qod.json?category=inspire") // fetching the quote of the day API
-.then(response => {
-    return response.json();
-})
-.then(json => {
-    // accesses the quote of the day from JSON API, and injects the quote and author into the HTML
-    let qod = json.contents.quotes[0];
-    const quoteBox = document.querySelector('#qod p');
-    const quoteCite = document.querySelector("cite");
-    quoteBox.textContent = qod.quote;
-    quoteCite.textContent = qod.author;
-})
-.catch(err => {
-    const quoteBox = document.querySelector("#qod p");
-    quoteBox.textContent = "Error:" + err;
-});
-
 const textarea = document.querySelector("textarea");
 
+const apikey = '4OO6ZTC7J8Y4BQ1V';
+let stock_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=FDX&interval=5min&apikey='+apikey;
+
+fetch(stock_URL) // fetching the stock price API
+  .then(response => {
+      return response.json();
+  })
+  .then(json => {
+      let lastRefreshed = json['Meta Data']["3. Last Refreshed"];
+      let fedExQuote = json["Time Series (5min)"][lastRefreshed];
+
+      const quoteBox = document.querySelector('#stock-price p');
+      const quoteCite = document.querySelector("cite");
+      quoteBox.textContent = JSON.stringify(fedExQuote);
+      quoteCite.textContent = "Last Refreshed:" + lastRefreshed;
+  })
+  .catch(err => {
+      const quoteBox = document.querySelector("#stock-price p");
+      quoteBox.textContent = "Error:" + err;
+  });
+
 window.addEventListener("load", () => {
-    chrome.storage.local.get("note", (item) => {
-        textarea.value = item.note;
-    })
+    let notes = "";
+    let userName = "";
+    
+    chrome.storage.local.get('note', function (result) {
+        notes = result.note;
+        console.log(result.note);
+        
+        if(notes == ""){
+            textarea.textContent = "";
+        } else {
+            textarea.textContent = notes;
+        }
+    });
+
+    chrome.storage.local.get('username', function (result) {
+        userName = result.username;
+        console.log(result.username);
+
+        if(userName == ""){
+            let newUser = prompt("Please enter your name", "New User");
+            chrome.storage.local.set({'username': newUser}, function() {
+              // Notify that we saved.
+              console.log('username \"'+ newUser + '\" saved')
+            });
+
+            greeting.textContent = 'Good '+ displayTimeOfDay() + "! " + newUser;
+        } else {
+            greeting.textContent = 'Good '+ displayTimeOfDay() + "! " + userName;
+        }
+    });
 });
 
 textarea.addEventListener("focusout", function() {
     const textareaValue = textarea.value;
-    chrome.storage.local.set({ "note": textareaValue }, function () {
+    chrome.storage.local.set({ 'note': textareaValue }, function () {
         console.log('note saved!')
     })
+});
+
+chrome.tabs.onCreated.addListener(function (tab){
+    while(tab.status!=="complete"){
+        console.log('wait for tab to load');
+    }
+    console.log('Tab loaded');
+    chrome.windows.get(tab.windowId, function(window){
+        if(window.type === "popup"){
+            console.log("This tab is a popup");
+            console.log("Query tabs");
+            let queryInfo = {
+                windowId: tab.windowId
+            };
+            chrome.tabs.query(queryInfo, function(tabs){
+                for (let tab of tabs) {
+                // tab.url requires the `tabs` permission
+                console.log(tab.id + tab.url + tab.windowId);
+                }   
+            });
+            console.log("All tabs");
+            chrome.tabs.query({active: true}, function(tabs){
+                for (let tab of tabs) {
+                // tab.url requires the `tabs` permission
+                console.log(tab.id + tab.url + tab.windowId);
+                }   
+            });   
+        }
+    });
 });
